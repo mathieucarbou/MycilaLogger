dec5c7380e53fda86a14b4c0830351efc51d010b
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-01-21 16:36:16 +0100

    Initial commit

---
+++ src/MycilaLogger.cpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright (C) 2023-2024 Mathieu Carbou and others
+ */
+#include "MycilaLogger.h"
+
+namespace Mycila {
+  LoggerClass Logger;
+} // namespace Mycila
