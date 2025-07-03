f791962d3231b0adca26e9f581f94a94297a58b0
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-07-03 19:32:34 +0200

    Updated sample

--- examples/Logger/Logger.ino
+++ examples/Logger/Logger.ino
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <MycilaLogger.h>
+#include <StreamString.h>
 
 Mycila::Logger logger;
+StreamString logBuffer;
 
 void setup() {
   Serial.begin(115200);
@@ -11,17 +13,26 @@ void setup() {
   Mycila::Logger::redirectArduinoLogs(logger);
 
   logger.setLevel(ARDUHAL_LOG_LEVEL_DEBUG);
-  logger.forwardTo(&Serial);
+
+  logger.forwardTo(&logBuffer);
 
   log_d("Arduino debug message");
   log_i("Arduino info message");
   log_w("Arduino warning message");
   log_e("Arduino error message");
 
+  Serial.println("Captured Arduino logs:");
+  Serial.println(logBuffer);
+  logBuffer.clear();
+
   logger.debug("APP", "A debug message");
   logger.info("APP", "An info message");
   logger.warn("APP", "A warning message");
   logger.error("APP", "An error message");
+
+  Serial.println("Captured Mycila logger logs:");
+  Serial.println(logBuffer);
+  logBuffer.clear();
 }
 
 void loop() {
